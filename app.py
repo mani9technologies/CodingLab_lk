48acda1e27a32d3b71351d8ab9ffaa5039fe409f
Ronald Kelechi <96204229+ronaldkelechi11@users.noreply.github.com>
2022-06-10 11:06:59 +0100

    Pyscript Failure.#3

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-<py-script>
-print("Hello World")
-</py-script>
+#Import section
+console.log('It\'s Connected')
+
+# Variable Declaration
+bigText = document.getElementById('maintext')
+button = document.getElementById('outlined')
+angle = 90
+rotateValue = bigText.style.transform
+rotateSum = rotateValue + 'scale(2)'
+
+# console logging
+console.log(angle)
+
+
+def scaling(*args):
+    console.log('Scaling commencing at angle')
+    bigText.setAttribute('style','transform: scale(2)')
+    bigText.setAttribute('style','transition: 3s')
