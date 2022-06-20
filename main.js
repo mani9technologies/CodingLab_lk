eb4ea897dcd5bbf6dba00cbbc9b067675a06554d
Ronald Kelechi <96204229+ronaldkelechi11@users.noreply.github.com>
2022-06-20 12:45:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-console.log("Hello World")
+console.log("Script Connection Succesful");
 
+// Variable Declarations
+var angle = 360;
+var bigText = document.getElementById("maintext");
+
+var rotateSum;
+var rotateTime;
+
+
+function vito(){
+
+var rotateValue = bigText.style.transform;
+var rotateTrans = bigText.style.transition;
+
+// Button Read out
+console.log("Button Click Succesfull");
+
+//Variable arithimetic
+rotateSum = rotateValue + "rotate(" + angle +"deg)" ;
+rotateTime = rotateTrans + "4s";
+
+//Checking if the value is true
+bigText.style.transform = rotateSum;
+bigText.style.transition = rotateTime;
+
+}
